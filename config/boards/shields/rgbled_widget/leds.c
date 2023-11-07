1a255bd81741529cf20d147a34f2317a4f1fc890
Tejas <94415-tejassaple@users.noreply.gitlab.com>
2023-11-08 00:35:32 +0530

    Moved layer led code inside CONFIG_ZMK_SPLIT_ROLE_CENTRAL if

--- config/boards/shields/rgbled_widget/leds.c
+++ config/boards/shields/rgbled_widget/leds.c
@@ -71,6 +71,23 @@ static int led_profile_listener_cb(const zmk_event_t *eh) {
 // run led_profile_listener_cb on BLE profile change (on central)
 ZMK_LISTENER(led_profile_listener, led_profile_listener_cb);
 ZMK_SUBSCRIPTION(led_profile_listener, zmk_ble_active_profile_changed);
+
+static int led_layer_listener_cb(const zmk_event_t *eh) {
+    uint8_t layer = ((struct zmk_layer_state_changed *)eh)->layer;
+    bool state = ((struct zmk_layer_state_changed *)eh)->state;
+    if (layer != _zmk_keymap_layer_default && state) {
+        LOG_INF("Default layer not selected, blinking magenta");
+
+        struct blink_item blink = {.duration_ms = CONFIG_RGBLED_WIDGET_BATTERY_BLINK_MS,
+                                   .color = LED_MAGENTA};
+        k_msgq_put(&led_msgq, &blink, K_NO_WAIT);
+    }
+}
+
+// run led_layer_listener_cb on layer change (on central)
+ZMK_LISTENER(led_layer_listener, led_layer_listener_cb);
+ZMK_SUBSCRIPTION(led_layer_listener, zmk_layer_state_changed);
+
 #else
 static int led_peripheral_listener_cb(const zmk_event_t *eh) {
     struct blink_item blink = {.duration_ms = CONFIG_RGBLED_WIDGET_OUTPUT_BLINK_MS};
@@ -90,22 +107,6 @@ ZMK_LISTENER(led_peripheral_listener, led_peripheral_listener_cb);
 ZMK_SUBSCRIPTION(led_peripheral_listener, zmk_split_peripheral_status_changed);
 #endif
 #endif // IS_ENABLED(CONFIG_ZMK_BLE)
-
-static int led_layer_listener_cb(const zmk_event_t *eh) {
-    uint8_t layer = ((struct zmk_layer_state_changed *)eh)->layer;
-    bool state = ((struct zmk_layer_state_changed *)eh)->state;
-    if (layer != _zmk_keymap_layer_default && state) {
-        LOG_INF("Default layer not selected, blinking magenta");
-
-        struct blink_item blink = {.duration_ms = CONFIG_RGBLED_WIDGET_BATTERY_BLINK_MS,
-                                   .color = LED_MAGENTA};
-        k_msgq_put(&led_msgq, &blink, K_NO_WAIT);
-    }
-}
-
-// run led_layer_listener_cb on layer change (on central)
-ZMK_LISTENER(led_layer_listener, led_layer_listener_cb);
-ZMK_SUBSCRIPTION(led_layer_listener, zmk_layer_state_changed);
 
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING)
 static int led_battery_listener_cb(const zmk_event_t *eh) {
